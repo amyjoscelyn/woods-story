dba2e4ce9c9316852b1161ff298bb697e9e5a34c
amyjoscelyn <amy.joscelyn@flatironschool.com>
2016-03-13 22:12:23 -0400

    deleted old, obsolete AMY classes (AMYChoice, AMYStory, etc.); added implementation to gameplay methods in AMYMainTableViewController

--- DreamGame/DreamGame/AMYMainTableViewController.m
+++ DreamGame/DreamGame/AMYMainTableViewController.m
@@ -11,15 +11,17 @@
 
 @interface AMYMainTableViewController ()
 
-@property (nonatomic, strong) NSMutableArray *mainStorypoints;
-@property (nonatomic, strong) NSMutableArray *branchingOptions;
-@property (nonatomic, strong) NSMutableArray *choices;
-@property (nonatomic, strong) NSMutableArray *effects;
-@property (nonatomic) BOOL endingTriggered;
+//@property (nonatomic, strong) NSMutableArray *mainStorypoints;
+//@property (nonatomic, strong) NSMutableArray *branchingOptions;
+//@property (nonatomic, strong) NSMutableArray *choices;
+//@property (nonatomic, strong) NSMutableArray *effects;
+//@property (nonatomic) BOOL endingTriggered;
 
 @property (nonatomic, strong) AMYStoryDataStore *dataStore;
 @property (strong, nonatomic) Question *currentQuestion;
 @property (strong, nonatomic) NSArray *sortedChoices;
+
+@property (nonatomic) CGFloat textHue;
 
 @end
 
@@ -34,18 +36,18 @@
     
     [self.dataStore fetchData];
     
-    self.currentQuestion = self.dataStore.currentQuestion;
+    [self setCurrentQuestionOfStory:self.dataStore.playthrough.currentQuestion];
     //self.sortedChoices = [self.dataStore.currentQuestion.choiceOuts sortedArrayUsingDescriptors:@[self.dataStore.sortByStoryIDAsc]];
 }
 
-- (void)setCurrentQuestion:(Question *)currentQuestion
+- (void)setCurrentQuestionOfStory:(Question *)currentQuestion
 {
     _currentQuestion = currentQuestion;
     _sortedChoices = [currentQuestion.choiceOuts sortedArrayUsingDescriptors:@[self.dataStore.sortByStoryIDAsc]];
     
-    _dataStore.currentQuestion = currentQuestion;
+    _dataStore.playthrough.currentQuestion = currentQuestion;
     
-    [_dataStore saveContext]; //for some reason, data is not persisting between sessions
+    [_dataStore saveContext];
 }
 
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
@@ -87,15 +89,17 @@
     NSInteger section = indexPath.section;
     NSInteger row = indexPath.row;
     
-    NSUInteger purple = 300;
-    CGFloat textHue = purple/359.0;
+    NSUInteger green = 230;
+    CGFloat textHue = green/359.0;
     
     if (section == 0)
     {
         cell.textLabel.text = self.currentQuestion.content;
-        cell.textLabel.textColor = [UIColor colorWithHue:textHue saturation:1.0 brightness:0.25 alpha:1.0];
-        cell.detailTextLabel.hidden = YES;
+        cell.textLabel.textColor = [UIColor colorWithHue:self.textHue saturation:1.0 brightness:0.25 alpha:1.0];
         cell.textLabel.numberOfLines = 0;
+        
+        cell.detailTextLabel.hidden = YES;
+        
         cell.userInteractionEnabled = NO;
         tableView.rowHeight = UITableViewAutomaticDimension;
         tableView.estimatedRowHeight = 45;
@@ -109,6 +113,18 @@
         {
             Choice *choice = self.sortedChoices[row];
 //            NSLog(@"choice: %@", choice);
+            
+           /* if (choice.prerequisites.count > 0)
+            {
+                ZhuLi *zhuLi = [ZhuLi new];
+                
+                //                NSLog(@"CHOICE PREREQ: %@", choice.prerequisites);
+                for (Prerequisite *prereq in choice.prerequisites)
+                {
+                    [zhuLi checkPrerequisite:prereq];
+                }
+            } */
+            
             cell.textLabel.text = choice.content;
         }
         else if (self.currentQuestion.questionAfter)
@@ -149,18 +165,48 @@
     
     NSUInteger row = indexPath.row;
     
+    /* ZhuLi *zhuLi = [ZhuLi new];
+    
+    if (self.currentQuestion.effects.count > 0)
+    { //this takes care of effects the currentQuestion might incur
+        for (Effect *thing in self.currentQuestion.effects)
+        {
+            [zhuLi doThe:thing];
+        }
+    } */
+    
     if (self.currentQuestion.questionAfter)
     {
-        self.currentQuestion = self.currentQuestion.questionAfter;
-        //self.sortedChoices = [self.dataStore.currentQuestion.choiceOuts sortedArrayUsingDescriptors:@[self.dataStore.sortByStoryIDAsc]];
+        [self setCurrentQuestionOfStory:self.currentQuestion.questionAfter];
     }
-    else if (self.dataStore.currentQuestion.choiceOuts.count > 0)
+    else if (self.dataStore.playthrough.currentQuestion.choiceOuts.count > 0)
     {
         Choice *selectedChoice = self.sortedChoices[row];
-        self.currentQuestion = selectedChoice.questionOut;
-    } else {
-        self.currentQuestion = self.dataStore.questions[0];
+        
+        /* if (selectedChoice.effects.count)
+        {
+            for (Effect *thing in selectedChoice.effects)
+            {
+                if ([thing.stringValue isEqualToString:@""])
+                {
+                    Choice *selectedChoice = self.sortedChoices[row];
+                    thing.stringValue = selectedChoice.content;
+                }
+                [zhuLi doThe:thing];
+            }
+        } */
+        
+        [self setCurrentQuestionOfStory:selectedChoice.questionOut];
+    }
+    else
+    {
+        [self setCurrentQuestionOfStory:self.dataStore.questions[0]];
         // go to next chapter or restart
+        
+        // below resets the properties
+        //I don't have any properties that need to be changed yet; this is good for effect properties, mostly
+        
+        [_dataStore saveContext];
     }
     
     [self.tableView reloadData];
